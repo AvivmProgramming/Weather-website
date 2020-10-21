e5a307ecf25e30b04b67687f8f941f0cdd5df265
BuildTools <unconfigured@null.spigotmc.org>
2020-10-21 17:06:55 +0300

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const hbs = require('hbs')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 
 
 // Define Paths for express config 
@@ -105,6 +105,6 @@ app.get('*' ,(req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
